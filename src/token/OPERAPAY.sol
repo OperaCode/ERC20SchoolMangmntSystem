dcae4c0a45501750637f06a081c27df23b9f65c5
Raphael Faboyinde <raphaelfaboyinde27@gmail.com>
2026-02-21 19:54:10 +0100

    variablesRename

--- src/token/OPERAPAY.sol
+++ src/token/OPERAPAY.sol
@@ -4,36 +4,36 @@ pragma solidity ^0.8.20;
 import {IERC20} from "./IERC20.sol";
 
 contract OPERAPAY is IERC20 {
-    // -------- Token Metadata --------
-    string private constant _name = "OPERAPAY";
-    string private constant _symbol = "OPPY";
-    uint8 private constant _decimals = 18;
+    // Token Metadata 
+    string private constant tokenName = "OPERAPAY";
+    string private constant tokenSymbol = "OPPY";
+    uint8 private constant tokenDecimal = 18;
 
-    uint256 private _totalSupply = 26_000_000 * 1e18;
+    uint256 private tokenTotalSupply = 26_000_000 * 1e18;
 
     
     mapping(address => uint256) private _balances;
     mapping(address => mapping(address => uint256)) private _allowances;
 
     constructor() {
-        _balances[msg.sender] = _totalSupply;
-        emit Transfer(address(0), msg.sender, _totalSupply);
+        _balances[msg.sender] = tokenTotalSupply;
+        emit Transfer(address(0), msg.sender, tokenTotalSupply);
     }
 
     function name() external pure returns (string memory) {
-        return _name;
+        return tokenName;
     }
 
     function symbol() external pure returns (string memory) {
-        return _symbol;
+        return tokenSymbol;
     }
 
     function decimals() external pure returns (uint8) {
-        return _decimals;
+        return tokenDecimal;
     }
 
     function totalSupply() external view returns (uint256) {
-        return _totalSupply;
+        return tokenTotalSupply;
     }
 
     function balanceOf(address user) external view returns (uint256) {
@@ -44,7 +44,7 @@ contract OPERAPAY is IERC20 {
         return _allowances[owner][spender];
     }
 
-    // -------- ERC20 Core --------
+   
     function transfer(address to, uint256 amount) external returns (bool) {
         _transfer(msg.sender, to, amount);
         return true;
@@ -70,7 +70,7 @@ contract OPERAPAY is IERC20 {
         return true;
     }
 
-    // -------- Internal Logic --------
+   
     function _transfer(address from, address to, uint256 amount) internal {
         require(from != address(0), "Transfer from zero");
         require(to != address(0), "Transfer to zero");
